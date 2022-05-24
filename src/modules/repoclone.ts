7d16dd013569a84feb99320fe268e0f128712a60
9bany <bany.y0599@gmail.com>
2022-05-24 11:07:37 +0700

    lint fixed

--- src/modules/repoclone.ts
+++ src/modules/repoclone.ts
@@ -1,30 +1,27 @@
 import {spawn} from 'child_process'
 import CONSTANT from './constant'
 
-async function repositoryClone(url, path = '') {
+async function repositoryClone(url, path = ''): Promise<any> {
   return new Promise((resolve, reject) => {
     if (!url) {
       reject(CONSTANT.URL_EMPTY)
+    } else {
+      const arrayCommand = `clone ${url} ${path}`.trim().split(' ')
+
+      const listen = spawn(`git`, arrayCommand)
+
+      listen.stdout.on('data', function () {
+        resolve(CONSTANT.CLONE_SUCCESS)
+      })
+
+      listen.on('exit', function () {
+        resolve('exited')
+      })
+
+      listen.stderr.on('data', function () {
+        resolve(CONSTANT.CLONE_SUCCESS)
+      })
     }
-
-    const arrayCommand = `clone ${url} ${path}`.trim().split(' ')
-
-    const listen = spawn(`git`, arrayCommand)
-
-    listen.stdout.on('data', function (a) {
-      resolve(CONSTANT.CLONE_SUCCESS)
-      return
-    })
-
-    listen.on('exit', function () {
-      resolve('exited')
-      return
-    })
-
-    listen.stderr.on('data', function (a) {
-      resolve(CONSTANT.CLONE_SUCCESS)
-      return
-    })
   })
 }
 
